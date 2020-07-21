0aa5148182f5bcdce6e04a694186166fc25e57c2
Enrique Gonzalez <enrique@saucelabs.com>
2020-07-21 15:50:33 -0700

    add node code

---
+++ test/utils.js
@@ -0,0 +1,20 @@
+const brokenCapabilities = {
+    'browserName': 'googlechrome',
+    'platformName': 'macOS 10.15',
+    'browserVersion': 'latest',
+    'sauce:options': {
+        'name': 'Broken Google Search'
+    }
+};
+
+const workingCapabilities = {
+    'browserName': 'googlechrome',
+    'platformName': 'macOS 10.15',
+    'browserVersion': 'latest',
+    'sauce:options': {
+        'name': 'Broken Google Search'
+    }
+};
+
+exports.brokenCapabilities = brokenCapabilities
+exports.workingCapabilities = workingCapabilities
